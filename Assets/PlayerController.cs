a592cadc4657575ec8736d9c00a6cfed02c646c9
Theodore <theodore.adamopoulos@gmail.com>
2021-11-13 22:40:45 +0200

    first commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float MovementSpeed = 4.5f;
+	public float JumpForce = 3.5f;
+
+
+	private Rigidbody2D _rigidbody;
+ 
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal"); 
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        if (Input.GetButtonDown("Jump")) {
+        	_rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
